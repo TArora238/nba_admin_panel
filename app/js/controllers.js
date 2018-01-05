995822e7d22f7a81e8cd80fa922b3bbfc5d39a20
Tushar Arora <tarora238@gmail.com>
2018-01-05 17:12:17 +0530

    artist apis chd

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -908,7 +908,7 @@
                 $.post(api.url + "verify_artist",{
                     access_token: localStorage.getItem('adminToken'),
                     artist_id: vm.artist.artist_id,
-                    serving_areas:vm.areas||'2'
+                    serving_areas:vm.areas||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1345,7 +1345,7 @@
 
                 $.post(api.url + "cancelled_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'2'
+                    area_id: localStorage.getItem('area_id')||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1409,7 +1409,7 @@
 
                 $.post(api.url + "finished_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'2'
+                    area_id: localStorage.getItem('area_id')||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1479,7 +1479,7 @@
 
                 $.post(api.url + "ongoing_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'2'
+                    area_id: localStorage.getItem('area_id')||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1567,7 +1567,7 @@
 
                 $.post(api.url + "paid_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'2'
+                    area_id: localStorage.getItem('area_id')||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1636,7 +1636,7 @@
 
                 $.post(api.url + "tobeaccepted_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'2'
+                    area_id: localStorage.getItem('area_id')||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1764,7 +1764,7 @@
 
                 $.post(api.url + "upcoming_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'2'
+                    area_id: localStorage.getItem('area_id')||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1859,7 +1859,7 @@
 
                 $.post(api.url + "categories_list",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'2'
+                    area_id: localStorage.getItem('area_id')||'1'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1962,7 +1962,7 @@
                     vm.cat.order_id = 1;
                 }
                 form.append("access_token", localStorage.getItem('adminToken'));
-                form.append("area_id", selected_area||'2');
+                form.append("area_id", selected_area||'1');
                 form.append("category_name", vm.cat.category_name);
                 form.append("category_description", vm.cat.category_description);
                 form.append("order_id", vm.cat.order_id);
