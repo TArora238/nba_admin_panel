3dffcbd124c6ab873a37fc7b3579571c20ac9743
Tushar Arora <tarora238@gmail.com>
2018-01-06 15:41:46 +0530

    dashboard london

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -88,7 +88,7 @@
       vm.dashboard = {};
         $.post(api.url + "dashboard",{
             access_token: localStorage.getItem('adminToken'),
-            area_id: localStorage.getItem('area_id')||'1'
+            area_id: localStorage.getItem('area_id')||'2'
         })
             .success(function(data, status) {
                 cfpLoadingBar.complete();
@@ -930,7 +930,7 @@
                 $.post(api.url + "verify_artist",{
                     access_token: localStorage.getItem('adminToken'),
                     artist_id: vm.artist.artist_id,
-                    serving_areas:vm.areas||'1'
+                    serving_areas:vm.areas||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1367,7 +1367,7 @@
 
                 $.post(api.url + "cancelled_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'1'
+                    area_id: localStorage.getItem('area_id')||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1431,7 +1431,7 @@
 
                 $.post(api.url + "finished_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'1'
+                    area_id: localStorage.getItem('area_id')||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1501,7 +1501,7 @@
 
                 $.post(api.url + "ongoing_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'1'
+                    area_id: localStorage.getItem('area_id')||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1589,7 +1589,7 @@
 
                 $.post(api.url + "paid_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'1'
+                    area_id: localStorage.getItem('area_id')||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1658,7 +1658,7 @@
 
                 $.post(api.url + "tobeaccepted_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'1'
+                    area_id: localStorage.getItem('area_id')||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1786,7 +1786,7 @@
 
                 $.post(api.url + "upcoming_bookings",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'1'
+                    area_id: localStorage.getItem('area_id')||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1881,7 +1881,7 @@
 
                 $.post(api.url + "categories_list",{
                     access_token: localStorage.getItem('adminToken'),
-                    area_id: localStorage.getItem('area_id')||'1'
+                    area_id: localStorage.getItem('area_id')||'2'
                 })
                     .success(function(data, status) {
                         cfpLoadingBar.complete();
@@ -1984,7 +1984,7 @@
                     vm.cat.order_id = 1;
                 }
                 form.append("access_token", localStorage.getItem('adminToken'));
-                form.append("area_id", selected_area||'1');
+                form.append("area_id", selected_area||'2');
                 form.append("category_name", vm.cat.category_name);
                 form.append("category_description", vm.cat.category_description);
                 form.append("order_id", vm.cat.order_id);
